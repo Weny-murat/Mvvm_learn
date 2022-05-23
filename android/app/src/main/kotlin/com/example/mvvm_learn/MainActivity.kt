d24a990e2386272047b6f0886859d95bf4b9cf42
Weny-murat <wenymurat@zohomail.com>
2022-05-23 17:50:31 +0300

    Preparing to learn mvvm

---
+++ android/app/src/main/kotlin/com/example/mvvm_learn/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mvvm_learn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_learn/presentation/resources/theme_manager.dart';
+
+class MyApp extends StatefulWidget {
+  // ignore: prefer_const_constructors_in_immutables
+  MyApp._internal(); //private named constructor default yani default constructor yerine kullanılacak.
+
+  static final MyApp instance =
+      MyApp._internal(); //single instance yani singleton yapma işlemi
+
+  factory MyApp() =>
+      instance; // factory ile default constructor yerine kullanacağımızı söylüyoruz.
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: getApplicationTheme(),
+    );
+  }
+}
