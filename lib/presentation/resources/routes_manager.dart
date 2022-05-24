9374763bf6dbf353ec9983f383511259fa49e41b
Weny-murat <wenymurat@zohomail.com>
2022-05-24 17:42:07 +0300

    Route prepare

--- lib/presentation/resources/routes_manager.dart
+++ lib/presentation/resources/routes_manager.dart
@@ -1,3 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_learn/presentation/login/login.dart';
+import 'package:mvvm_learn/presentation/onboarding/onboarding.dart';
+import 'package:mvvm_learn/presentation/splash/splash.dart';
+
 class Routes {
   static const String loginRoute = '/login';
   static const String splashRoute = '/';
@@ -6,4 +11,18 @@ class Routes {
   static const String registerRoute = '/register';
   static const String mainRoute = '/main';
   static const String storeDetailsRoute = '/storeDetails';
+}
+
+class RouteGenerator {
+  static Route<dynamic> getRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case Routes.splashRoute:
+        return MaterialPageRoute(builder: (_) => SplashView());
+      case Routes.loginRoute:
+        return MaterialPageRoute(builder: (_) => LoginView());
+      case Routes.onBoardingRoute:
+        return MaterialPageRoute(builder: (_) => OnBoardingView());
+      default:
+    }
+  }
 }
