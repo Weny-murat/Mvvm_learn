4e59b2466e5a55db8d09c40fc0bf2ebb19663979
Weny-murat <wenymurat@zohomail.com>
2022-05-26 17:00:03 +0300

    Onboarding Etc.

---
+++ lib/presentation/resources/assets_manager.dart
@@ -0,0 +1,5 @@
+const String imagesPath = 'assets/images';
+
+class ImageAssets {
+  static const String splashlogo = '$imagesPath/splash_logo.png';
+}
--- lib/presentation/splash/splash.dart
+++ lib/presentation/splash/splash.dart
@@ -1,15 +1,50 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:mvvm_learn/presentation/resources/assets_manager.dart';
+import 'package:mvvm_learn/presentation/resources/color_manager.dart';
+import 'package:mvvm_learn/presentation/resources/routes_manager.dart';
 
 class SplashView extends StatefulWidget {
-  SplashView({Key? key}) : super(key: key);
+  const SplashView({Key? key}) : super(key: key);
 
   @override
   State<SplashView> createState() => _SplashViewState();
 }
 
 class _SplashViewState extends State<SplashView> {
+  // ignore: unused_field
+  Timer? _timer;
+
+  _startDelay() {
+    _timer = Timer(const Duration(seconds: 3), _goNext);
+  }
+
+  _goNext() {
+    Navigator.pushReplacementNamed(context, Routes.onBoardingRoute);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startDelay();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer?.cancel();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: ColorManager.primary,
+      body: const Center(
+        child: Image(
+          image: AssetImage(ImageAssets.splashlogo),
+        ),
+      ),
+    );
   }
 }
